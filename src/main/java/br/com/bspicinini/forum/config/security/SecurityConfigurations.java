187208ea5d947984697888b6e081abf11b23dc34
Bruno de Souza Picinini <brunosouzapicinini@gmail.com>
2021-07-10 11:29:44 -0300

    Autorizacao com base em roles

--- src/main/java/br/com/bspicinini/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/bspicinini/forum/config/security/SecurityConfigurations.java
@@ -46,6 +46,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
                 .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO Usuario(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$ZIm2Kw48/QbINOecuEV4yeJKhwFdii6iPPmQV64luScObMZU4fFY2');
+INSERT INTO Usuario(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$ZIm2Kw48/QbINOecuEV4yeJKhwFdii6iPPmQV64luScObMZU4fFY2');
+
+INSERT INTO PERFIL(id, nome) VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (2,2);
 
 INSERT INTO Curso(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO Curso(nome, categoria) VALUES('HTML 5', 'Front-end');
